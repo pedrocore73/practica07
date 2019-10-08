84bcfd8919d3dbb8f935266af9386c6ea2d4f47f
pedrocore73 <pjimenez@corenetworks.es>
2019-10-08 20:35:26 +0200

    seis

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ app.get('/productos', function (req, res){
     res.sendFile(__dirname + '/public/productos.html');
 });
 
+app.get('/contacto', function (req, res){
+    res.sendFile(__dirname + '/public/contacto.html');
+});
+
+app.use(function(req, res){
+    res.status(404);
+    res.sendFile(__dirname + '/public/pag404.html');
+});
+
 app.listen(8888, function(){
     console.log('Servidor escuchando en http://localhost:8888');
 });
